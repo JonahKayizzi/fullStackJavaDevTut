58ad350228608aa9df868701cc0d96f77a8eb45a
Jonah Kayizzi <jonahkayizzi@gmail.com>
2022-04-26 16:19:20 +0300

    swithcase

---
+++ swithcase char/src/hello.java
@@ -0,0 +1,30 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char x = 'B';
+		
+		switch (x) {
+		case 'a':
+		case 'A':
+			System.out.println("Value of x is a");
+			break;
+		case 'b':
+		case 'B':
+			System.out.println("Value of x is b");
+			break;
+		case 'c':
+		case 'C':
+			System.out.println("Value of x is c");
+			break;
+		default :
+			System.out.println("Value of x is neither a, b or c");
+			break;
+		}
+		
+
+	}
+
+}

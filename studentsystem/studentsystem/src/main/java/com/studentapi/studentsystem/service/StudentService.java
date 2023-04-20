e9a4cabee815f79b714f04ee5426efa8adb0ec0d
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-04-20 19:11:45 +0300

    Implement controller actions and api endpoints for add and displaying students

--- studentsystem/studentsystem/src/main/java/com/studentapi/studentsystem/service/StudentService.java
+++ studentsystem/studentsystem/src/main/java/com/studentapi/studentsystem/service/StudentService.java
@@ -1,4 +1,10 @@
 package com.studentapi.studentsystem.service;
 
+import com.studentapi.studentsystem.model.Student;
+
+import java.util.List;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
